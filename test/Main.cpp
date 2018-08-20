3fcbf1de18292d4d36ec61466c2234864d454025
Jan D''Orgeville <jdorgeville@spiria.com>
2018-08-20 17:58:26 -0400

    libzip 1.5.1

--- test/Main.cpp
+++ test/Main.cpp
@@ -9,14 +9,37 @@
 #include <windows.h>
 #include <Shlwapi.h>
 
-#include "..\zlib\inc\zlib.h"
+#include "..\libzip\inc\zip.h"
 
 int main()
 {
 
 #if 1
-    BYTE buff[32];
-    crc32(123, buff, 32);
+    //Open the ZIP archive
+    int err = 0;
+    zip *z = zip_open("foo.zip", 0, &err);
+
+    //Search for the file of given name
+    const char *name = "file.txt";
+    struct zip_stat st;
+    zip_stat_init(&st);
+    zip_stat(z, name, 0, &st);
+
+    //Alloc memory for its uncompressed contents
+    char *contents = new char[st.size];
+
+    //Read the compressed file
+    zip_file *f = zip_fopen(z, name, 0);
+    zip_fread(f, contents, st.size);
+    zip_fclose(f);
+
+    //And close the archive
+    zip_close(z);
+
+    //Do something with the contents
+    //delete allocated memory
+    delete[] contents;
+
 
 #else
     if (!loadAPI()) return EXIT_FAILURE;
